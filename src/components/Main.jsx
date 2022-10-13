30effe8724488f6fc6a9eb13237ecd3a341d34b4
root <root@DESKTOP-M366OAF.localdomain>
2022-10-13 11:40:35 -0400

    wrote fetch function

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,6 +2,25 @@ import React from "react";
 import {Navbar} from "./"
 
 const Main = () => {
+  
+async function fetchPuppies () {
+  try {
+    let playerId = 7441
+    const fetchedPuppies = await fetch('https://fsa-puppy-bowl.herokuapp.com/api/2209-FTB-ET-WEB-FT/players');
+    const puppies = await fetchedPuppies.json();
+    const fetchOnePuppy = await fetch(`https://fsa-puppy-bowl.herokuapp.com/api/2209-FTB-ET-WEB-FT/players/${playerId}`)
+        const puppyData = await fetchOnePuppy.json();
+    const onePuppy = puppyData.data.player
+  } catch (err) {
+    console.error(err);
+  }
+}
+fetchPuppies() 
+
+
+  
+  
+  
   return (
     <div id="main">
       <Navbar />
